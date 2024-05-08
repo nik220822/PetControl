2f7a77e4260b8a4deda000c70c948fa7515cbb33
Nikolai Pavlov <nik220822@rambler.ru>
2024-05-08 16:47:20 +0300

    Controller was fixed, newId was moved to the model

--- src/main/java/com/nicode/ibsspring/controller/Controller.java
+++ src/main/java/com/nicode/ibsspring/controller/Controller.java
@@ -5,20 +5,18 @@ import com.nicode.ibsspring.logic.PetModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class Controller {
     private static final PetModel petModel = PetModel.getInstance();
-    private static final AtomicInteger newId = new AtomicInteger(1);
 
     @PostMapping(value = "/createPet", consumes = "application/json")
     public static String creatPet(@RequestBody Pet pet) {
-        petModel.add(pet, newId.getAndIncrement());
-        if (newId.get() == 1) {
+        int id = petModel.add(pet);
+        if (id == 1) {
             return "Поздравляем, вы создали своего первого питомца!";
         } else {
-            return "Питомец № " + newId.get() + " успешно содан";
+            return "Питомец № " + id + " успешно содан";
         }
     }
 
@@ -33,17 +31,20 @@ public class Controller {
     }
 
     @DeleteMapping(value = "/deletePet", consumes = "application/json", produces = "application/json")
-    public String deletePet(@RequestBody Map<String, Integer> map) {
-        petModel.getAll().remove(map.get("id"));
-        return "Питомец" + map.get("id") + " был удалён";
+    public String deletePet(@RequestBody Map<String, String> map) {
+        if (!petModel.getAll().containsKey(Integer.parseInt(map.get("id")))) {
+            return "Питомца с id = " + map.get("id") + " не найдено";
+        }
+        petModel.getAll().remove(Integer.parseInt(map.get("id")));
+        return "Питомец с id = " + map.get("id") + " был удалён";
     }
 
     @PutMapping(value = "/putPet", consumes = "application/json", produces = "application/json")
     public String putPet(@RequestBody Map<String, String> map) {
         if (!petModel.getAll().containsKey(Integer.parseInt(map.get("id")))) {
-            return "Питомца с id= " + map.get("id") + "не найдено";
+            return "Питомца с id = " + map.get("id") + " не найдено";
         }
         petModel.getAll().put(Integer.parseInt(map.get("id")), new Pet(map.get("name"), map.get("type"), Integer.parseInt(map.get("age"))));
-        return "Питомец" + map.get("id") + " был успешно изменён";
+        return "Питомец " + map.get("id") + " был успешно изменён";
     }
 }
--- src/main/java/com/nicode/ibsspring/logic/PetModel.java
+++ src/main/java/com/nicode/ibsspring/logic/PetModel.java
@@ -3,10 +3,13 @@ package com.nicode.ibsspring.logic;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PetModel implements Serializable {
     private static final PetModel instance = new PetModel();
     private final Map<Integer, Pet> model;
+
+    private static final AtomicInteger newId = new AtomicInteger(1);
 
     public PetModel() {
         model = new HashMap<Integer, Pet>();
@@ -16,8 +19,10 @@ public class PetModel implements Serializable {
         return instance;
     }
 
-    public void add(Pet pet, int id) {
+    public int add(Pet pet) {
+        int id= newId.getAndIncrement();
         model.put(id, pet);
+        return id;
     }
 
     public Pet getFromList(int id) {
